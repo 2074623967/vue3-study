1c64e3d30b6f18406b18d16fdc1cd60546b22b6b
tangchungjiao <2074623967@qq.com>
2023-02-12 10:02:36 +0800

    vue-router的基本使用和配置

--- 08_learn_vuerouter/src/App.vue
+++ 08_learn_vuerouter/src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <p>
+      <router-link to="/home">首页</router-link>
+      <router-link to="/about">关于</router-link>
+    </p>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
 }
 </script>
 
---
+++ 08_learn_vuerouter/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+//配置映射关系
+const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: Home },
+  { path: '/about', component: About },
+];
+//创建一个路由对象router
+const router = createRouter({
+  routes,
+  history: createWebHashHistory(),
+});
+export default router;
