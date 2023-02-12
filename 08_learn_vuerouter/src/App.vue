7b57fa1a5074ce8a93da2d3fed552ea41646e2e4
tangchungjiao <2074623967@qq.com>
2023-02-12 15:35:59 +0800

    vue-router的router-link/router-view插槽和v-slot

--- 08_learn_vuerouter/src/App.vue
+++ 08_learn_vuerouter/src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="app">
     <p>
-      <router-link to="/home">首页</router-link>
+      <!-- props: href 跳转的链接 -->
+      <!-- props: route对象 -->
+      <!-- props: navigate导航函数 -->
+      <!-- props: isActive 是否当前处于活跃的状态 -->
+      <!-- props: isExactActive 是否当前处于精确的活跃状态 -->
+      <!--custom表示自定义元素,原本的a元素不见了 -->
+      <router-link to="/home" v-slot="props" custom>
+        <NavBar title="title" />
+        <button @click="props.navigate">{{props.href}}</button>
+        <button @click="props.navigate">哈哈哈</button>
+        <span :class="{'active': props.isActive}">{{props.isActive}}</span>
+        <span :class="{'active': props.isActive}">{{props.isExactActive}}</span>
+        <!-- <p>{{props.route}}</p> -->
+      </router-link>
       <router-link to="/about">关于</router-link>
       <router-link to="/user/123/info/why">用户</router-link>
     </p>
-    <router-view />
+    <router-view v-slot="props">
+      <transition name="why">
+        <keep-alive>
+          <component :is="props.Component"></component>
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'
 export default {
   name: 'App',
-  components: {},
+  components: { NavBar },
 }
 </script>
 
@@ -24,5 +44,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.why-enter-from,
+.why-leave-to {
+  opacity: 0;
+}
+
+.why-enter-active,
+.why-leave-active {
+  transition: opacity 1s ease;
 }
 </style>
