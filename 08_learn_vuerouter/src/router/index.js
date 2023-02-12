7ee94da61da0329221c932847126187d1a60c9ec
tangchungjiao <2074623967@qq.com>
2023-02-12 19:28:49 +0800

    vue-router导航守卫的使用

--- 08_learn_vuerouter/src/router/index.js
+++ 08_learn_vuerouter/src/router/index.js
@@ -38,6 +38,10 @@ const routes = [
     component: () => import('../pages/User.vue'),
   },
   {
+    path: '/login',
+    component: () => import('../pages/Login.vue'),
+  },
+  {
     path: '/:patchMatch(.*)',
     name: 'notfound',
     component: () => import('../pages/NotFound.vue'),
@@ -62,6 +66,30 @@ router.addRoute(categoryRoute);
 router.addRoute('home', {
   path: 'moment',
   component: () => import('../pages/HomeMoment.vue'),
+});
+
+// 导航守卫beforeEach
+let counter = 0;
+// to: Route对象, 即将跳转到的Route对象
+// from: Route对象,
+/**
+ * 返回值问题:
+ *    1.false: 不进行导航
+ *    2.undefined或者不写返回值: 进行默认导航
+ *    3.字符串: 路径, 跳转到对应的路径中
+ *    4.对象: 类似于 router.push({path: "/login", query: ....})
+ */
+router.beforeEach((to, from) => {
+  console.log(`进行了${++counter}路由跳转`);
+  // if (to.path.indexOf("/home") !== -1) {
+  //   return "/login"
+  // }
+  if (to.path !== '/login') {
+    const token = window.localStorage.getItem('token');
+    if (!token) {
+      return '/login';
+    }
+  }
 });
 
 export default router;
