550dc608bc6ffb1060aa8f84ce3517a434a55975
tangchungjiao <2074623967@qq.com>
2023-02-12 13:16:43 +0800

    vue-router参数补充+动态路由匹配

--- 08_learn_vuerouter/src/App.vue
+++ 08_learn_vuerouter/src/App.vue
@@ -3,6 +3,7 @@
     <p>
       <router-link to="/home">首页</router-link>
       <router-link to="/about">关于</router-link>
+      <router-link to="/user/123/info/why">用户</router-link>
     </p>
     <router-view />
   </div>
--- 08_learn_vuerouter/src/router/index.js
+++ 08_learn_vuerouter/src/router/index.js
@@ -3,11 +3,25 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 //配置映射关系
 const routes = [
   { path: '/', redirect: '/home' },
-  { path: '/home', component: () => import('../pages/Home.vue') },
+  {
+    path: '/home',
+    name: 'home',
+    component: () =>
+      import(/*webpackChunkName: "home-chuck"*/ '../pages/Home.vue'),
+  },
   {
     path: '/about',
+    name: 'about',
     component: () => import('../pages/About.vue'),
-    meta: { name: 'why', age: 18 },
+    meta: {
+      name: 'why',
+      age: 18,
+    },
+  },
+  {
+    path: '/user/:id/info/:name',
+    name: 'user',
+    component: () => import('../pages/User.vue'),
   },
 ];
 //创建一个路由对象router
