5771ae60bb3f21edc5af70731fcd487f6aa92abd
tangchungjiao <2074623967@qq.com>
2023-02-15 17:05:07 +0800

    useState的封装

--- 09_learn_vuex/src/App.vue
+++ 09_learn_vuex/src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import Home from './pages/Home.vue'
+import Home from './pages/03_useState封装后使用.vue'
 export default {
   name: 'App',
   components: {
---
+++ 09_learn_vuex/src/pages/03_useState封装后使用.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Home:{{ $store.state.counter }}</h2>
+    <hr />
+    <h2>Home:{{ counter}}</h2>
+    <h2>Home:{{name }}</h2>
+  </div>
+</template>
+
+<script>
+import { useState } from '../hooks/useState'
+export default {
+  setup() {
+    const storeState = useState(['counter', 'name'])
+    const storeState2 = useState({
+      sCounter: (state) => state.counter,
+      sName: (state) => state.name,
+    })
+
+    return {
+      ...storeState,
+      ...storeState2,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
